d1e23dfc394d5e1a4ee73bc8e488bb9e0125d01a
lizaa-t <lizaa.t@ya.ru>
2022-11-14 18:40:12 +0600

    replace db path with the absolute one

--- data/db/manager.py
+++ data/db/manager.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
-engine = create_engine("sqlite:///qpa_final_project.db", echo=True)
+engine = create_engine(
+    "sqlite:////home/liza/PycharmProjects/qpa_final_project/data/db/qpa_final_project.db",
+    echo=True,
+)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
