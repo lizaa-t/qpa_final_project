e126d7dc582bd6eede63d1af86cd699bf4f23abb
lizaa-t <lizaa.t@ya.ru>
2022-11-17 03:10:12 +0600

    split logic by separate files

--- gc_content_functions.py
+++ gc_content_functions.py
@@ -3,33 +3,6 @@ from collections import Counter
 
 import matplotlib.pyplot as plt
 
-from data.db.queries import dna_to_rna, codon_to_aminoacid
-from data.db.models import CODON_LENGTH
-
-
-def convert_dna_to_rna(dna_sequence: str) -> str:
-    """Converts DNA sequence into RNA"""
-    rna_sequence = ""
-    dna_sequence = dna_sequence.upper()
-
-    for nucleotide in dna_sequence:
-        rna_sequence += dna_to_rna(nucleotide)
-
-    return rna_sequence
-
-
-def convert_rna_to_protein(rna_sequence: str) -> str:
-    """Converts RNA sequence into protein"""
-    protein = ""
-    rna_sequence = rna_sequence.upper()
-
-    for i in range(0, len(rna_sequence), CODON_LENGTH):
-        codon = rna_sequence[i:i+CODON_LENGTH]
-        if len(codon) == CODON_LENGTH:
-            protein += codon_to_aminoacid(codon)
-
-    return protein
-
 
 def calculate_gc_ratio(dna_sequence: str, step: int = 100) -> list[float]:
     """Calculates G-C content metric for DNA subsequences with a given step"""
---
+++ gene_expression_functions.py
@@ -0,0 +1,25 @@
+from data.db.queries import dna_to_rna, codon_to_aminoacid
+from data.db.models import CODON_LENGTH
+
+
+def convert_dna_to_rna(dna_sequence: str) -> str:
+    """Converts DNA sequence into RNA"""
+    rna_sequence = ""
+    dna_sequence = dna_sequence.upper()
+    for nucleotide in dna_sequence:
+        rna_sequence += dna_to_rna(nucleotide)
+
+    return rna_sequence
+
+
+def convert_rna_to_protein(rna_sequence: str) -> str:
+    """Converts RNA sequence into protein"""
+    protein = ""
+    rna_sequence = rna_sequence.upper()
+
+    for i in range(0, len(rna_sequence), CODON_LENGTH):
+        codon = rna_sequence[i:i+CODON_LENGTH]
+        if len(codon) == CODON_LENGTH:
+            protein += codon_to_aminoacid(codon)
+
+    return protein
