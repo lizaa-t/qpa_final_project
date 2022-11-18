054332e70a307ac005915c50b9c28385a8a2646f
lizaa-t <lizaa.t@ya.ru>
2022-11-19 03:44:43 +0600

    add config

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+
+APP_DIR = os.getenv("APP_DIR")
+PROJECT_DIR = os.path.abspath(APP_DIR)
+
+SAVE_DIR_NAME = "pic"
+SAVE_DIR = os.path.join(PROJECT_DIR, SAVE_DIR_NAME)
+
+DB_URI = f"sqlite:///{APP_DIR}/db/qpa_final_project.db"
