fb35a6c03dc3ec0a138964287ec60c78e887cd7e
lizaa-t <lizaa.t@ya.ru>
2022-11-19 04:31:57 +0600

    add main.py for app usage example

---
+++ app/main.py
@@ -0,0 +1,7 @@
+# your imports here, e.g.:
+from gene_expression_functions import convert_dna_to_rna
+
+
+if __name__ == "__main__":
+    # your code here, e.g.:
+    assert convert_dna_to_rna('ATTTGGCTACTAACAATCTA') == 'AUUUGGCUACUAACAAUCUA'
