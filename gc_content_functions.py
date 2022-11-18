2a4617a10f361a6f885fb742f7eadc0cfd1bf022
lizaa-t <lizaa.t@ya.ru>
2022-11-18 23:12:59 +0600

    add titles to plot

--- gc_content_functions.py
+++ gc_content_functions.py
@@ -27,6 +27,9 @@ def plot_gc_ratio(gc_metric: list[float],
     x_values = [x for x in range(0, sequence_length, step)]
 
     figure, axis_x = plt.subplots()
+    axis_x.set(title="GC-content per sequence",
+               xlabel="Sequence start position",
+               ylabel="GC-content, %",)
     axis_x.plot(x_values, gc_metric)
 
     plt.savefig(os_path_join("pic", f"{filename}.{file_format}"))
