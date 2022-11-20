39dec774f27d6adaa277c56f22f4291600e8c9ae
lizaa-t <lizaa.t@ya.ru>
2022-11-21 05:55:08 +0600

    add validators for DNA and RNA

--- app/constants.py
+++ app/constants.py
@@ -1,2 +1,13 @@
+from pydantic import constr
+
+
 TRANSLATION_STOP_SIGNAL = "."
 CODON_LENGTH = 3
+
+DNA_BASES = ("A", "C", "G", "T")
+RNA_BASES = ("A", "C", "G", "U")
+
+# determines string consisting only from DNA_BASES: as a whole, ignoring case
+DNA_CONSTRAINT = constr(regex=f"(?i)^[{', '.join(DNA_BASES)}]+$")
+# same for RNA
+RNA_CONSTRAINT = constr(regex=f"(?i)^[{', '.join(RNA_BASES)}]+$")
