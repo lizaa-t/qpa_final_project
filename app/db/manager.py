7027af8470266b9567fd69ceaa52332b5b13dd8b
lizaa-t <lizaa.t@ya.ru>
2022-11-20 22:23:40 +0600

    remove echo mode from db engine

--- app/db/manager.py
+++ app/db/manager.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 import app.config as config
 
 
-engine = create_engine(config.DB_URI, echo=True)
+engine = create_engine(config.DB_URI)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
