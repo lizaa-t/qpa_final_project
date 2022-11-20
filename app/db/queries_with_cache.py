1be97620a04ebe695230f313070f9f27f092b55d
lizaa-t <lizaa.t@ya.ru>
2022-11-20 21:34:51 +0600

    add queries version with caching

---
+++ app/db/queries_with_cache.py
@@ -0,0 +1,24 @@
+from functools import cache as functools_cache
+
+from app.db.models import Aminoacid, DNABase, RNABase, RNATriplet
+from app.db.manager import Session
+
+
+@functools_cache
+def dna_to_rna(nucleotide: str) -> str:
+    """Retrieve RNA base by given DNA base"""
+    with Session() as session:
+        rna_base = session.query(RNABase.base).\
+            join(DNABase, DNABase.rna_id == RNABase.id).\
+            filter(DNABase.base == nucleotide).one().base
+        return rna_base
+
+
+@functools_cache
+def codon_to_aminoacid(codon: str) -> str:
+    """Retrieve aminoacid by given codon"""
+    with Session() as session:
+        aminoacid = session.query(Aminoacid.aminoacid).\
+            join(RNATriplet, RNATriplet.aminoacid_id == Aminoacid.id).\
+            filter(RNATriplet.triplet == codon).one().aminoacid
+        return aminoacid
