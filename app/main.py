4537b5394ae45068d239261292305eef098cf1b5
lizaa-t <lizaa.t@ya.ru>
2022-11-30 03:17:37 +0600

    add CLI to the main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,30 @@
-# your imports here, e.g.:
-from app.genomic_functions.gene_expression_functions import convert_dna_to_rna
+import argparse
+
+from app.genomic_functions.gene_expression import convert_dna_to_rna
+from app.genomic_functions.gene_expression import convert_rna_to_protein
+from app.genomic_functions.gc_content import calculate_gc_content
+from app.genomic_functions.gc_content import plot_gc_content
+
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("input", metavar="INPUT", type=str,
+                        help="The input DNA data")
+arg_parser.add_argument("step", nargs="?", type=int, default=100,
+                        help="The step for gc-content calculating")
+arg_parser.add_argument("filename", nargs="?", type=str,
+                        default="gc_ratio_plot",
+                        help="The filename of a gc-content plot")
 
 
 if __name__ == "__main__":
-    # your code here, e.g.:
-    assert convert_dna_to_rna('ATTTGGCTACTAACAATCTA') == 'AUUUGGCUACUAACAAUCUA'
+    args = arg_parser.parse_args()
+
+    dna_sequence = args.input
+    step = args.step
+    filename = args.filename
+
+    rna = convert_dna_to_rna(dna_sequence)
+    protein = convert_rna_to_protein(rna)
+
+    dna_gc = calculate_gc_content(dna_sequence, step)
+    plot_gc_content(dna_gc, step, filename=filename)
